51c41afba39a60675167f997b8d528466e93b80e
John Knotts <jhnnyk@gmail.com>
2025-07-07 16:13:40 -0600

    remove boilerplate and hook up bulma

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="has-text-centered">hello world</h1>
   </main>
 </template>
